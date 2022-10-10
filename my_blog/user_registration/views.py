375cbf0480fa5d7fb873f762d8da768572889e86
Akli Miklos <akli.miklos@gmail.com>
2022-10-10 17:44:51 +0200

    1

--- my_blog/user_registration/views.py
+++ my_blog/user_registration/views.py
@@ -27,6 +27,7 @@ def register(request):
             username = form.cleaned_data.get('username')
             usr_obj = User.objects.filter(username=username).first()
             prof_temp = ProfileModel.objects.create(user_id=usr_obj.pk)
+            
             messages.success(request, f"Your account has  been created! You are able")
             return redirect('login')
     else:
